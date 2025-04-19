87b8a04226d89ee2abe5dfadd587edfd6e3e4d98
aligh123 <fanava.itnovin@yahoo.com>
2025-04-19 00:12:10 -0700

    exercise2-5: i have extracted the components and imported them in course component and import it in app.jsx

---
+++ exercise2-5/src/Course.jsx
@@ -0,0 +1,19 @@
+import Header from "./Header";
+import Content from "./Content";
+import Total from "./Total";
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      {course.map((courseItem) => (
+        <div key={courseItem.id}>
+          <Header name={courseItem.name} />
+          <Content parts={courseItem.parts} />
+          <Total parts={courseItem.parts} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Course;
